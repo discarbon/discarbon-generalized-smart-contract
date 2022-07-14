494d376646ca4930a2af1c3a108ae9951ee215b0
haurog <36535774+haurog@users.noreply.github.com>
2022-07-14 10:59:26 +0200

    Adding some logic to forward function

--- contracts/Pooling.sol
+++ contracts/Pooling.sol
@@ -3,8 +3,7 @@ pragma solidity ^0.8.9;
 
 /// @title disCarbon Devcon 6 attendee pooling contract
 /// @author haurog, danceratopz
-/// @notice This contract exchanges the coins/tokens of the users for carbon tokens (NCT) and moves them forward to the pooling address.
-/// @dev All function calls are currently implemented without side effects
+/// @notice This contract exchanges the coins/tokens of the users for carbon tokens (NCT) and sends them to the pooling address.
 
 // Import this file to use console.log
 import "hardhat/console.sol";
@@ -12,10 +11,15 @@ import "hardhat/console.sol";
 contract Pooling {
     mapping(address => uint256) public contributors;
 
+    uint256 totalCarbonPooled = 0;
+
     address poolingAddress = 0x1c0AcCc24e1549125b5b3c14D999D3a496Afbdb1; // haurogs public address (for testing purposes)
 
     function exchangeCoinToCarbonToken() public payable {}  // Matic
     function exchangeTokenToCarbonToken() public {} // handles every ERC-20 allowed
     function swapToCarbonToken() private {} // does the swap
-    function forwardCarbonToken() private {} // forwards the tokens to the poolingAddress
+    function forwardCarbonToken(uint256 CarbonAmount) private { // forwards the tokens to the poolingAddress
+    totalCarbonPooled += CarbonAmount;
+    contributors[msg.sender] += CarbonAmount;
+    }
 }
