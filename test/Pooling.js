a43f924a65952380dd6ac3469367d1d8d376f6ea
haurog <36535774+haurog@users.noreply.github.com>
2022-08-03 10:52:24 +0200

    Test that the pooling contract deploys

---
+++ test/Pooling.js
@@ -0,0 +1,30 @@
+const {time, loadFixture} = require("@nomicfoundation/hardhat-network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
+
+const {constants, expectRevert} = require('@openzeppelin/test-helpers');
+
+describe("Pooling", function () {
+  // We define a fixture to reuse the same setup in every test.
+  // We use loadFixture to run this setup once, snapshot that state,
+  // and reset Hardhat Network to that snapshopt in every test.
+  async function deployPooling() {
+    // Contracts are deployed using the first signer/account by default
+    const [owner, otherAccount] = await ethers.getSigners();
+
+    const Pooling = await ethers.getContractFactory("Pooling");
+    const pooling = await Pooling.deploy();
+
+    return { pooling, owner, otherAccount };
+  }
+
+  describe("Deployment", function () {
+    it("Should deploy", async function () {
+      const {pooling, owner, otherAccount } = await loadFixture(deployPooling);
+
+      // console.log(pooling)
+
+      expect(pooling.address != constants.zeroAddress);
+    });
+  });
+});
