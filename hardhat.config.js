caf4fc171e234d0cf454f5677b9d1a1b105a4996
haurog <36535774+haurog@users.noreply.github.com>
2022-11-28 13:56:02 +0100

    Remove all third party contracts from dodoc generation

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,15 +2,6 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv/config")
 require('@primitivefi/hardhat-dodoc');  // creates docs from every time one compiles
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -41,4 +32,9 @@ module.exports = {
   etherscan: {
     apiKey: process.env.POLYGONSCAN_API_KEY
   },
+  dodoc: {
+    runOnCompile: true,
+    debugMode: false,
+    exclude: ['interfaces', 'types', '@openzeppelin'],
+  }
 };
