1d8f671a11ef65a31c3bd44ad9bd8b050d034742
danceratopz <danceratopz@gmail.com>
2023-02-03 08:43:36 +0100

    chore: add a script that tests with a fork from a recent block

---
+++ bin/test_recent_fork.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Run "hardhat test" using a local node forked from a very recent block.
+# 
+# Run this script from the project's root directory.
+#
+# Any additonal arguments to this script are forwarded as args to "hardhat test", for example:
+# bin/test_recent_fork.sh --grep WETH
+#
+# Requires jq
+# sudo apt install jq
+
+TEST_EXTRA_ARGS=$@
+
+POLYGON_RPC_URL=https://polygon-rpc.com
+
+LATEST_BLOCK_NUMBER=`curl -s $POLYGON_RPC_URL -X \
+    POST -H "Content-Type: application/json" \
+    --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' \
+    | jq  -r .result`;
+FORK_BLOCK_NUMBER=$(($LATEST_BLOCK_NUMBER - 0x40))
+
+echo -e "\nForking from block $FORK_BLOCK_NUMBER...\n"
+npx hardhat node --fork $POLYGON_RPC_URL --fork-block-number $FORK_BLOCK_NUMBER > /dev/null & 
+node_pid=$!
+node_exit_code=$?
+
+sleep 2
+
+test_output=`npx hardhat test --network localhost $TEST_EXTRA_ARGS 2>&1 | tee /dev/tty`
+test_exit_code=$?
+
+kill $!
+kill $node_pid
+
+if echo "$test_output" | grep -q "Uncaught error outside test suite"; then 
+    echo -e "\n\nUncaught error outside test suite - problem running 'hardhat node'?"; 
+    exit 1;
+else 
+    echo -e "\n\n'hardhat test' completed with exit code $test_exit_code"; 
+    exit $test_exit_code; 
+fi
